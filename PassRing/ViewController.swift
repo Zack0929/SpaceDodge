7bc51033081b1b69553cb8ed3d64f9f126530d27
石黒隼 <hayato@ishiguroshunnomacbook-air.local>
2018-05-05 16:34:34 +0900

    Initial Commit

---
+++ PassRing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassRing
+//
+//  Created by 石黒隼 on 2018/05/05.
+//  Copyright © 2018年 石黒隼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
